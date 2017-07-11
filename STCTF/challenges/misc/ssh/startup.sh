dfac30a24d00f587238ffd8295b406b838d03b3c
Joy Law <joylaw28@hotmail.com>
2017-07-11 15:23:45 +0800

    File Management
    Added Points

--- STCTF/challenges/misc/ssh/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker build -t sshcontainer .
-docker run -t -d -P --name sshcontainer sshcontainer
-docker port sshcontainer 22
-docker start sshcontainer
--- STCTF/challenges/misc/sshtunnel/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker build -t iwantix2 .
-docker run --name db-iwantix2 -e MYSQL_ROOT_PASSWORD=wow_w0w -e MYSQL_USER=iwantix2 -e MYSQL_PASSWORD=iwantix22 -e MYSQL_DATABASE=iwantix2 -v pathtosql/sql.sql:/docker-entrypoint-initdb.d/sql.sql -d mysql
-docker run --name iwantix2 --link db-iwantix2 -d -p 8002:80 -p 8001:22 iwantix2
-docker exec -ti iwantix2 bash
-service ssh start
